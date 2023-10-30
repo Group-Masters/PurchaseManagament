2591bbf172d153d75cccc4392ce54441253ca206
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2023-10-30 11:59:44 +0300

    Changes

---
+++ PurchaseManagament.Persistence/Concrete/Mappings/CurrenyMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurchaseManagament.Domain.Entities;
+
+namespace PurchaseManagament.Persistence.Concrete.Mappings
+{
+    public class CurrenyMapping : AuditableEntityMapping<Currency>
+    {
+        public override void ConfigureDerivedEntityMapping(EntityTypeBuilder<Currency> builder)
+        {
+            builder.ToTable("CURRENCIES");
+
+            builder.Property(x => x.Name)
+                .IsRequired().HasColumnName("CURRENCY_NAME").HasColumnType("nvarchar(20)");
+
+            builder.HasMany(x => x.Offers)
+                .WithOne(x => x.Currency)
+                .HasForeignKey(builder => builder.CurrencyId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ PurchaseManagament.Persistence/Concrete/Mappings/EmployeeRoleMapping.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurchaseManagament.Domain.Entities;
+
+namespace PurchaseManagament.Persistence.Concrete.Mappings
+{
+    public class EmployeeRoleMapping : AuditableEntityMapping<EmployeeRole>
+    {
+        public override void ConfigureDerivedEntityMapping(EntityTypeBuilder<EmployeeRole> builder)
+        {
+            builder.ToTable("EMPLOYEE_ROLES");
+
+            builder.Property(x => x.EmployeeId)
+                .HasColumnName("EMPLOYEE_ID");
+
+            builder.Property(x => x.RoleId)
+                .HasColumnName("ROLE_ID");
+
+            builder.HasOne(x => x.Employee)
+                .WithMany(x => x.EmployeeRoles)
+                .HasForeignKey(x => x.EmployeeId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.EmployeeRoles)
+                .HasForeignKey(x => x.RoleId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
