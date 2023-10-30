7d7dd5f03ca66076f984bca2b5c7ff956dc764bb
Hasan GÖKALP <Hasan GKALP@DESKTOP-R04PVQ3>
2023-10-30 10:30:36 +0300

    Changes

---
+++ PurchaseManagament.Persistence/Concrete/Mappings/OfferMapping.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurchaseManagament.Domain.Entities;
+
+namespace PurchaseManagament.Persistence.Concrete.Mappings
+{
+    public class OfferMapping : AuditableEntityMapping<Offer>
+    {
+        public override void ConfigureDerivedEntityMapping(EntityTypeBuilder<Offer> builder)
+        {
+            builder.ToTable("OFFERS");
+
+            builder.Property(x => x.CurrencyId)
+                .HasColumnName("CURRENCY_ID").HasColumnType("BigInt");
+
+            builder.Property(builder => builder.OfferedPrice)
+                .HasColumnName("OFFERED_PRICE").HasColumnType("BigInt");
+
+            builder.Property(builder => builder.RequestId)
+                .HasColumnName("REQUEST_ID").HasColumnType("BigInt");
+
+            builder.Property(builder => builder.StatusId)
+                .HasColumnName("STATUS_ID").HasColumnType("BigInt");
+
+            builder.Property(builder => builder.SupplierId)
+                .HasColumnName("SUPLIER_ID").HasColumnType("BigInt");
+
+            builder.HasOne(x => x.Supplier)
+                .WithMany(x => x.Offers)
+                .HasForeignKey(x => x.SupplierId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.ApprovingEmployee)
+                .WithMany(x => x.Offers)
+                .HasForeignKey(builder => builder.ApprovingEmployeeId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Status)
+                .WithMany(x => x.Offers)
+                .HasForeignKey(x => x.StatusId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Currency)
+                .WithMany(x => x.Offers)
+                .HasForeignKey(x => x.CurrencyId).OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(x => x.Request)
+                .WithMany (x => x.Offers).HasForeignKey(x => x.RequestId).OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ PurchaseManagament.Persistence/Concrete/Mappings/StatusMapping.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PurchaseManagament.Domain.Entities;
+using System.Resources;
+
+namespace PurchaseManagament.Persistence.Concrete.Mappings
+{
+    public class StatusMapping : AuditableEntityMapping<Status>
+    {
+        public override void ConfigureDerivedEntityMapping(EntityTypeBuilder<Status> builder)
+        {
+            builder.ToTable("STATUS");
+        }
+    }
+}
