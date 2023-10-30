0b27a96e809f30850bcdeda48a7380403b639592
Ahiskaa <sefa.ahiskaa@gmail.com>
2023-10-30 08:42:12 +0300

    Auditable oluşturuldu

---
+++ PurchaseManagament.Domain/Abstract/AuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseManagament.Domain.Abstract
+{
+    public class AuditableEntity : BaseEntity
+    {
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string? CreatedIP4 { get; set; }
+        public long? CreatedById { get; set; }
+        public long? ModifiedById { get; set; }
+        public string? ModifiedIP4 { get; set; }
+
+    }
+}
