167f6ed9e83d2c4a28d21a77bdc6068600b8d16e
Ahiskaa <sefa.ahiskaa@gmail.com>
2023-11-01 10:28:46 +0300

    ProductController kontrol edilecek

---
+++ PurchaseManagament.Application/Concrete/Models/Dtos/RequestDto.cs
@@ -0,0 +1,35 @@
+﻿using PurchaseManagament.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseManagament.Application.Concrete.Models.Dtos
+{
+    public class RequestDTO
+    {
+        public long ProductId { get; set; }
+
+        public long ApprovingEmployeeId { get; set; }
+        public long RequestEmployeeId { get; set; }
+        public string Details { get; set; }
+        public double Quantity { get; set; }
+
+        public virtual Status State { get; set; } // Durum
+
+        /* Status Durumlar
+         * 
+            Beklemede = 0,
+            Reddedildi = 1,
+            Onay = 2,
+            YönetimBekleme = 3,
+            YönetimOnay = 4,
+            YönetimRed = 5,
+            
+            
+            Tamamlandı = 9
+         
+         */
+    }
+}
