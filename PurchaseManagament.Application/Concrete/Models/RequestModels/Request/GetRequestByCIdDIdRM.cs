532d56a474513f8e1545ad9b14a987148f8b56a9
Eyyüp Hikmet <eyyup.hikmet@outlook.com>
2023-11-03 10:37:31 +0300

    GetRequestByEmployeeId ve GetRequestByCIdDId fonksiyonları eklendi

---
+++ PurchaseManagament.Application/Concrete/Models/RequestModels/Request/GetRequestByCIdDIdRM.cs
@@ -0,0 +1,8 @@
+﻿namespace PurchaseManagament.Application.Concrete.Models.RequestModels.Request
+{
+    public class GetRequestByCIdDIdRM
+    {
+        public Int64 CompanyId { get; set; }
+        public Int64 DepartmentId { get; set; }
+    }
+}
