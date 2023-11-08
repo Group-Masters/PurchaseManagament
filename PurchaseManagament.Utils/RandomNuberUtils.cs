0dfb87560be0f5436572c781f6990639169a6898
eren ağır <agir_eren@hotmail.com>
2023-11-08 08:52:46 +0300

    ranmdom sayı ve mail uthil oluşturuldu

---
+++ PurchaseManagament.Utils/RandomNuberUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseManagament.Utils
+{
+    public static class RandomNuberUtils
+    {
+        public static string CreateRandom(int startNumber, int EndNumber)
+        {
+            Random Random = new Random();
+            var result = Random.Next(startNumber, EndNumber);
+            return result.ToString();
+        }
+    }
+}
