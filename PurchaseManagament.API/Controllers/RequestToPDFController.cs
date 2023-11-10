41329b2b1b89947a1d91d57310a789186d33cdcc
Mustafa Yılman <Mustafa Ylman@DESKTOP-R04PVQ3>
2023-11-10 18:00:00 +0300

    PDF Controller Updates

--- PurchaseManagament.API/Controllers/RequestToPDFController.cs
+++ PurchaseManagament.API/Controllers/RequestToPDFController.cs
@@ -24,29 +24,29 @@ namespace PurchaseManagament.API.Controllers
         }
 
         [HttpPost("GenerateReportToPDFByCompany")]
-        public async Task GenerateReportToPDFByCompany(Int64 id)
+        public async Task GenerateReportToPDFByCompany([FromBody] GetByIdVM getByIdVM)
         {
-            await _reportToPdfService.GenerateReportToPDFByCompany(new GetByIdVM { Id = id });
+            await _reportToPdfService.GenerateReportToPDFByCompany(getByIdVM);
         }
 
 
         [HttpPost("GenerateReportToPDFByDepartman")]
-        public async Task GenerateReportToPDFByDepartman(Int64 id)
+        public async Task GenerateReportToPDFByDepartman([FromBody] GetByIdVM getByIdVM)
         {
-            await _reportToPdfService.GenerateReportToPDFByDepartman(new GetByIdVM { Id = id });
+            await _reportToPdfService.GenerateReportToPDFByDepartman(getByIdVM);
         }
 
         [HttpPost("GenerateReportToPDFByProduct")]
-        public async Task GenerateReportToPDFByProduct(GetReportProductVM getReportProductVM)
+        public async Task GenerateReportToPDFByProduct([FromBody] GetReportProductVM getReportProductVM)
         {
-            await _reportToPdfService.GenerateReportToPDFByProduct(new GetReportProductVM { CompanyId = getReportProductVM.CompanyId, ProductId = getReportProductVM.ProductId });
+            await _reportToPdfService.GenerateReportToPDFByProduct(getReportProductVM);
         }
 
 
         [HttpPost("GenerateReportToPDFBySupplier")]
-        public async Task GenerateReportToPDFBySupplier(Int64 id)
+        public async Task GenerateReportToPDFBySupplier([FromBody] GetByIdVM getByIdVM)
         {
-            await _reportToPdfService.GenerateReportToPDFBySupplier(new GetByIdVM { Id = id });
+            await _reportToPdfService.GenerateReportToPDFBySupplier(getByIdVM);
         }
     }
 }
