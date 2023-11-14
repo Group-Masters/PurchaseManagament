3e1913a785e686a3b602da61dc86cac9ba4654f6
Mustafa Yılman <Mustafa Ylman@DESKTOP-R04PVQ3>
2023-11-14 13:28:30 +0300

    ProductServices ProductImg src updates

--- PurchaseManagament.Application/Concrete/Services/ProductService.cs
+++ PurchaseManagament.Application/Concrete/Services/ProductService.cs
@@ -62,7 +62,7 @@ namespace PurchaseManagament.Application.Concrete.Services
         public async Task<Result<HashSet<ProductDto>>> GetAllProduct()
         {
             var result = new Result<HashSet<ProductDto>>();
-            var entities = _unitWork.GetRepository<Product>().GetAllAsync("MeasuringUnit");
+            var entities = _unitWork.GetRepository<Product>().GetAllAsync("MeasuringUnit", "ImgProduct");
             var mappedEntities = _mapper.Map<HashSet<ProductDto>>(await entities);
             result.Data = mappedEntities;
             return result;
