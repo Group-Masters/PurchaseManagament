4ab3d9aa0a347721dd92ca124cd9de0f0213d1e2
Ahiskaa <sefa.ahiskaa@gmail.com>
2023-11-14 09:51:02 +0300

    ImgProduct Güncellendi GetAll oluşturuldu

--- PurchaseManagament.API/Controllers/ImgProductController.cs
+++ PurchaseManagament.API/Controllers/ImgProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PurchaseManagament.Application.Abstract.Service;
 using PurchaseManagament.Application.Concrete.Models.RequestModels.ImgProduct;
+using PurchaseManagament.Application.Concrete.Services;
 using PurchaseManagament.Domain.Entities;
 
 namespace PurchaseManagament.API.Controllers
@@ -21,6 +22,13 @@ namespace PurchaseManagament.API.Controllers
             var entity = await _imgProductService.CreateImgProduct(ımgProduct);
 
             return Ok(entity);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAllImgProduct()
+        {
+            var entities = await _imgProductService.GetAllImgProduct();
+            return Ok(entities);
         }
     }
 }
